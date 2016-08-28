9ad4f70ab8c5a4f578bcebed7caae0e2c849da4f
Colin Williams <lackita@gmail.com>
2016-08-28 00:27:01 -0400

    added tabs and refactored main page into separate views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def toggle_focus_path(problem)
+    if problem.focus
+      unfocus_problem_path(problem)
+    else
+      focus_problem_path(problem)
+    end
+  end
 end
